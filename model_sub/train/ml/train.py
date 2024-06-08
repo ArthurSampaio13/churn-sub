11d31c59b3ed60ca61a4f238cd653316d9df725b
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-06-08 00:29:55 -0300

    feat: treinamento com tunning

--- model_sub/train/ml/train.py
+++ model_sub/train/ml/train.py
@@ -1,14 +1,22 @@
 # %%
 import pandas as pd
 
+
 import sqlalchemy
 import sqlite3
 
 
 from sklearn import model_selection
 from feature_engine import imputation
+
 from feature_engine import encoding
+
+
 from sklearn import ensemble
+from sklearn import tree
+from sklearn import linear_model
+
+
 from sklearn import pipeline
 from sklearn import metrics
 
@@ -69,25 +77,72 @@ imput_0 = imputation.ArbitraryNumberImputer(arbitrary_number=0, variables=missin
 # %%
 # OneHotEncoding
 onehot = encoding.OneHotEncoder(drop_last=True, variables=cat_features)
-# %%
-# MODEL
-model = ensemble.RandomForestClassifier(n_estimators=200, min_samples_leaf=50, n_jobs=-1)
 
-# %%
-# PIPELINE
-model_pipe = pipeline.Pipeline(steps=[("imput 0", imput_0),
-                                      ("imput -1", imput_1),
-                                      ("onehot", onehot),
-                                      ("Modelo", model)])
 # %%
 # REGEX para remover [^A-Za-z0-9_]+
 X_train.columns = X_train.columns.str.strip().str.replace('[^A-Za-z0-9_]+', '', regex=True)
+
 # %%
-model_pipe.fit(X_train, y_train)
+# MODELS
+rf_clf = ensemble.RandomForestClassifier(n_estimators=200, 
+                                         min_samples_leaf=50, 
+                                         n_jobs=-1, 
+                                         random_state=42)
+
+ada_clf = ensemble.AdaBoostClassifier(n_estimators=200, 
+                                      learning_rate=0.8, 
+                                      random_state=42)
+
+dt_clf = tree.DecisionTreeClassifier(max_depth=15,
+                                     min_samples_leaf=50,
+                                     random_state=42)
+
+rl_clf = linear_model.LogisticRegressionCV(cv=4, 
+                                           n_jobs=-1)
+# %%
+# PIPELINE COM GRIDSEARCH
+
+param_grid = {'n_estimators':[50,100,200,250],
+                                    'min_samples_leaf':[5,10,50,100]}
+
+grid_search = model_selection.GridSearchCV(rf_clf, 
+                             param_grid, 
+                             n_jobs=1, 
+                             cv=4, 
+                             scoring='roc_auc',
+                             verbose=3,
+                             refit=True)
+
+#{'min_samples_leaf': 5, 'n_estimators': 250}
+
+pipeline_rf = pipeline.Pipeline(steps=[("imput 0", imput_0),
+                                      ("imput -1", imput_1),
+                                      ("onehot", onehot),
+                                      ("Modelo", grid_search)])
+
+# %%
+
+def train_test_report(modelo, X_train, y_train, X_test, y_test, key_metric, is_prob=True):
+    modelo.fit(X_train, y_train)
+    pred = modelo.predict(X_test)
+    proba = modelo.predict_proba(X_test)
+    
+    metric_result = key_metric(y_test, proba[:,1]) if is_prob else key_metric(y_test, pred)
+    
+    return metric_result
+    
+
+# %%
+
+pipeline_rf.fit(X_train, y_train)
+
+# %%
+pd.DataFrame(grid_search.cv_results_).sort_values(by="rank_test_score")
+
 # %%
 # METRICAS
-y_train_pred = model_pipe.predict(X_train)
-y_train_prob = model_pipe.predict_proba(X_train)
+y_train_pred = pipeline_rf.predict(X_train)
+y_train_prob = pipeline_rf.predict_proba(X_train)
 
 # %%
 acc_train = round(100 * metrics.accuracy_score(y_train, y_train_pred), 2)
@@ -119,8 +174,8 @@ skplt.metrics.plot_lift_curve(y_train, y_train_prob)
 plt.show()
 # %%
 # TESTE
-y_test_pred = model_pipe.predict(X_test)
-y_test_prob = model_pipe.predict_proba(X_test)
+y_test_pred = pipeline_rf.predict(X_test)
+y_test_prob = pipeline_rf.predict_proba(X_test)
 
 acc_test = round(100 * metrics.accuracy_score(y_test, y_test_pred), 2)
 roc_test = round(100 * metrics.roc_auc_score(y_test, y_test_prob[:,1]), 2)
@@ -150,6 +205,20 @@ plt.show()
 skplt.metrics.plot_lift_curve(y_test, y_test_prob)
 plt.show()
 # %%
-# VARIAVEIS MAIS IMPORTANTES
-fs_importance = pd.DataFrame({"importabnce":model_pipe[-1].feature_importances_,
-                             "feature": X_train.columns.tolist()})
+skplt.metrics.plot_cumulative_gain(y_test,y_test_prob)
+plt.show()
+# %%
+### Testando na OOT para ver se descolou
+X_oot, y_oot = dt_oot[features], dt_oot[target]
+
+y_prob_oot = pipeline_rf.predict_proba(X_oot)
+
+roc_oot = round(100 * metrics.roc_auc_score(y_oot, y_prob_oot[:,1]), 2)
+
+print("roc_train", roc_oot)
+
+skplt.metrics.plot_lift_curve(y_oot, y_prob_oot)
+plt.show()
+# %%
+skplt.metrics.plot_cumulative_gain(y_oot,y_prob_oot)
+plt.show()
